f78d9f7bbcb616f8e75d98ba3e6297fe5eae11f7
Collen Maphike <collenmaphike25@gmail.com>
2020-09-08 12:34:27 +0200

    internal error resolved

--- server/routes/edit.js
+++ server/routes/edit.js
@@ -1,19 +1,23 @@
 const StudentsModel = require('../models/students');
 const express = require('express');
 const router = express.Router();
+const { hashPassword } = require('../security/passwordManager');
 
 
 const edit = (router) => {
   router.put('/edit/:id', async (req, res) => {
 
-    const { name, surname, email, homeAddress, hashedPassword, idNumber } = req.body;
+    const { name, surname, email, homeAddress, password, idNumber } = req.body;
+    const hashedPassword = await hashPassword(password)
+
     const toBeEdited = { name, surname, email, homeAddress, hashedPassword, idNumber }
     try {
       console.log('toBeEdited', toBeEdited)
-      await StudentsModel.findAndModify({ _id: req.params.id }), toBeEdited, { new: true }, function (err, toBeEdited) {
-        res.send(toBeEdited)
-      }
+      await StudentsModel.findByIdAndUpdate({ _id: req.params.id }, toBeEdited, { new: true })
+      res.send(toBeEdited)
+
     } catch (err) {
+      console.log(err)
       res.send(500)
     }
 
